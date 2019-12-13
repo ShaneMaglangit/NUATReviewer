bb92e1711d32bc07f1700e35d630928c6233166c
ShaneMaglangit <shanedmaglangit@gmail.com>
2019-12-13 12:53:22 +0800

    Added Randomized Choices

--- app/src/main/java/com/shanemaglangit/nuatreviewer/ui/question/QuestionViewModel.kt
+++ app/src/main/java/com/shanemaglangit/nuatreviewer/ui/question/QuestionViewModel.kt
@@ -20,6 +20,27 @@ class QuestionViewModel(title: String) : ViewModel() {
     val currentQuestion: LiveData<Question>
         get() = _currentQuestion
 
+    private var _optionTopLeft = MutableLiveData<String>()
+    val optionTopLeft: LiveData<String>
+        get() = _optionTopLeft
+
+    private var _optionTopRight = MutableLiveData<String>()
+    val optionTopRight: LiveData<String>
+        get() = _optionTopRight
+
+    private var _optionBottomLeft = MutableLiveData<String>()
+    val optionBottomLeft: LiveData<String>
+        get() = _optionBottomLeft
+
+    private var _optionBottomRight = MutableLiveData<String>()
+    val optionBottomRight: LiveData<String>
+        get() = _optionBottomRight
+
+    val optionTopLeftChecked = MutableLiveData<Boolean>()
+    val optionTopRightChecked = MutableLiveData<Boolean>()
+    val optionBottomRightChecked = MutableLiveData<Boolean>()
+    val optionBottomLeftChecked = MutableLiveData<Boolean>()
+
     init {
         db.collection("questions")
             .whereEqualTo("topic", title.toLowerCase())
@@ -36,6 +57,19 @@ class QuestionViewModel(title: String) : ViewModel() {
 
     fun selectRandomQuestion() {
         val selectedQuestion = questions[Random.nextInt(0, questionsCount)]
+        val choices = selectedQuestion.options.toMutableList().apply{
+            add(selectedQuestion.answer)
+            shuffle()
+        }
+
         _currentQuestion.value = selectedQuestion
+        _optionTopLeft.value = choices[0]
+        _optionTopRight.value = choices[1]
+        _optionBottomLeft.value = choices[2]
+        _optionBottomRight.value = choices[3]
+    }
+
+    fun submitAnswer() {
+
     }
 }
